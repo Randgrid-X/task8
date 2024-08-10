9361d789cceb4f0938a1c5b5be30a335ddd5c778
Randgrid-X <randgrid13@gmail.com>
2024-08-10 15:56:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_sorted = sorted(students)
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 a = sum(grades[0])/len(grades[0])
 b = sum(grades[1])/len(grades[1])
 c = sum(grades[2])/len(grades[2])
 d = sum(grades[3])/len(grades[3])
 e = sum(grades[4])/len(grades[4])
-journal = dict(zip(students, (a, b, c, d, e)))
+journal = dict(zip(students_sorted, (a, b, c, d, e)))
 print(journal)
